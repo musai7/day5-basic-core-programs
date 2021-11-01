b0dd53b9abefa5368062ad531f102fad5ec8f78f
musai7 <musaishankar307@gmail.com>
2021-11-01 19:58:58 +0530

    added prime factors of a number

---
+++ src/com/day5/basicprograms/PrimeFactorsOfNumber.java
@@ -0,0 +1,26 @@
+package com.day5.basicprograms;
+
+import java.util.Scanner;
+
+public class PrimeFactorsOfNumber {
+
+	public static void main(String[] args) {
+		int temp = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number ");
+		int num = sc.nextInt();
+		
+		System.out.print("prime factors of " + num + " are : ");
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				temp++;
+			}
+			if (temp > 0) {
+				System.out.print(" " + i);
+			}
+			temp = 0;
+		}
+
+	}
+
+}
