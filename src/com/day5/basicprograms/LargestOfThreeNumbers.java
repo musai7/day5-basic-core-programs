fd55bc7e08ebee1c80fd45c11faadd32e5e1f904
musai7 <musaishankar307@gmail.com>
2021-11-01 21:26:05 +0530

    modified largest of three numbers

--- src/com/day5/basicprograms/LargestOfThreeNumbers.java
+++ src/com/day5/basicprograms/LargestOfThreeNumbers.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class LargestOfThreeNumbers {
 	public static void main(String[] args) {
-		System.out.println("enter three numbers num1 num2 num3 ");
-		Scanner sc =new Scanner(System.in);
-		int num1=sc.nextInt();
-		int num2=sc.nextInt();
-		int num3=sc.nextInt();
-		if(num1>num2 && num1>num3) {
-			System.out.println("num1 is gtreater than all "+num1);
-		}
-		else if(num2>num3) {
-			System.out.println("num2 is gtreater than all "+num2);
-		}
-		else
-			System.out.println("num3 is gtreater than all "+num3);
-		
+		int num1 = (int) (Math.random() * 100);
+		System.out.println("number 1 : " + num1);
+		int num2 = (int) (Math.random() * 100);
+		System.out.println("number 2 : " + num2);
+		int num3 = (int) (Math.random() * 100);
+		System.out.println("number 3 : " + num3);
+		if (num1 > num2 && num1 > num3) {
+			System.out.println("num1 is gtreater than all " + num1);
+		} else if (num2 > num3) {
+			System.out.println("num2 is gtreater than all " + num2);
+		} else
+			System.out.println("num3 is gtreater than all " + num3);
+
 	}
 }
